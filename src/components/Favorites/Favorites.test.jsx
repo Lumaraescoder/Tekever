ec7c55f8235130bf222f1c632474eceaddffb758
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2022-09-23 13:34:39 +0100

    Favorites started

---
+++ src/components/Favorites/Favorites.test.jsx
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Favorites from './Favorites';
+
+
+
+
+const client = new QueryClient();
+
+describe("Favorites", () => {
+  it("renders a without crashing", () => {
+    const component = render(
+      <QueryClientProvider client={client}>
+        <Favorites />
+      </QueryClientProvider>
+    );
+    expect(component).toBeTruthy();
+  });
+
+
+});
